ee4f09c794c92d99507d0e4f9152d8908432ad6e
Idan <idansano10@gmail.com>
2025-10-27 10:17:44 +0200

    added condition to validate_guesses

--- hangman/game.py
+++ hangman/game.py
@@ -5,6 +5,8 @@ def init_state(secret: str, max_tries: int) -> dict:
     return new_dict
 
 def validate_guess(ch: str, guessed: set[str]) -> tuple[bool, str]:
+    if len(ch) != 1:
+        raise ValueError("You must type only one letter")
     if ch in guessed:
         return False, f"{ch} has already been deciphered "
     return True, f"{ch} was not guessed"
@@ -14,4 +16,7 @@ def apply_guess(state: dict, ch: str) -> bool:
     if ch in state["secret"]:
         return True
     return False
+
+# def is_won(state: dict) -> bool:
+
 
